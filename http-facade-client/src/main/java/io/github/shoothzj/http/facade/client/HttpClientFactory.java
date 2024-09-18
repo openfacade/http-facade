ad9a91bc38aaece43969130924c8ba670ab9bb3d
ZhangJian He <shoothzj@gmail.com>
2024-09-18 19:55:21 +0800

    refactor: remove reflection usage
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- http-facade-client/src/main/java/io/github/shoothzj/http/facade/client/HttpClientFactory.java
+++ http-facade-client/src/main/java/io/github/shoothzj/http/facade/client/HttpClientFactory.java
@@ -1,30 +1,22 @@
 package io.github.shoothzj.http.facade.client;
 
-import java.lang.reflect.InvocationTargetException;
-
 public class HttpClientFactory {
     public static HttpClient createHttpClient(HttpClientConfig httpClientConfig) {
-        try {
-            HttpClient client;
-            switch (httpClientConfig.engine()) {
-                case OKHTTP:
-                    client = (HttpClient) Class.forName("io.github.shoothzj.http.facade.client.OkhttpClient")
-                            .getDeclaredConstructor(HttpClientConfig.class).newInstance(httpClientConfig);
-                    break;
-                case ASYNC_HTTP_CLIENT:
-                    client = (HttpClient) Class.forName("io.github.shoothzj.http.facade.client.AsyncHttpClient")
-                            .getDeclaredConstructor(HttpClientConfig.class).newInstance(httpClientConfig);
-                    break;
-                default:
-                    client = (HttpClient) Class.forName("io.github.shoothzj.http.facade.client.JdkHttpClient")
-                            .getDeclaredConstructor(HttpClientConfig.class).newInstance(httpClientConfig);
-                    break;
-            }
-
-            return client;
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
-                 InvocationTargetException e) {
-            throw new IllegalStateException("Failed to create HttpClient for engine: " + httpClientConfig.engine(), e);
+        HttpClient client;
+        switch (httpClientConfig.engine()) {
+            case ASYNC_HTTP_CLIENT:
+                client = new AsyncHttpClient(httpClientConfig);
+                break;
+            case JDK:
+                client = new JdkHttpClient(httpClientConfig);
+                break;
+            case OKHTTP:
+                client = new OkhttpClient(httpClientConfig);
+                break;
+            default:
+                throw new IllegalStateException("Unsupported HttpClient engine: " + httpClientConfig.engine());
         }
+
+        return client;
     }
 }
--- http-facade-server/src/main/java/io/github/shoothzj/http/facade/server/HttpServerFactory.java
+++ http-facade-server/src/main/java/io/github/shoothzj/http/facade/server/HttpServerFactory.java
@@ -1,25 +1,18 @@
 package io.github.shoothzj.http.facade.server;
 
-import java.lang.reflect.InvocationTargetException;
-
 public class HttpServerFactory {
     public static HttpServer createHttpServer(HttpServerConfig httpServerConfig) {
-        try {
-            HttpServer server;
-            switch (httpServerConfig.engine()) {
-                case Jetty:
-                    server = (HttpServer) Class.forName("io.github.shoothzj.http.facade.server.JettyHttpServer")
-                            .getDeclaredConstructor(HttpServerConfig.class).newInstance(httpServerConfig);
-                    break;
-                default:
-                    server = (HttpServer) Class.forName("io.github.shoothzj.http.facade.server.VertxHttpServer")
-                            .getDeclaredConstructor(HttpServerConfig.class).newInstance(httpServerConfig);
-                    break;
-            }
-            return server;
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | NoSuchMethodException |
-                 InvocationTargetException e) {
-            throw new IllegalStateException("Failed to create HttpServer for engine: " + httpServerConfig.engine(), e);
+        HttpServer server;
+        switch (httpServerConfig.engine()) {
+            case Jetty:
+                server = new JettyHttpServer(httpServerConfig);
+                break;
+            case Vertx:
+                server = new VertxHttpServer(httpServerConfig);
+                break;
+            default:
+                throw new IllegalStateException("Unsupported HttpServer engine: " + httpServerConfig.engine());
         }
+        return server;
     }
 }
