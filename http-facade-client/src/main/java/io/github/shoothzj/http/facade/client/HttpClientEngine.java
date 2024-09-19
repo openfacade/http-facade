99da30654fe05812a5f9d092d8982f45aac4b579
ZhangJian He <shoothzj@gmail.com>
2024-09-19 08:35:34 +0800

    feat: support jdk8 HttpURLConnection
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- http-facade-client/src/main/java/io/github/shoothzj/http/facade/client/HttpClientEngine.java
+++ http-facade-client/src/main/java/io/github/shoothzj/http/facade/client/HttpClientEngine.java
@@ -2,6 +2,9 @@ package io.github.shoothzj.http.facade.client;
 
 public enum HttpClientEngine {
     ASYNC_HTTP_CLIENT,
+    // jdk 11+ HttpClient
     JDK,
+    // jdk 8+ HttpURLConnection
+    JDK8,
     OKHTTP,
 }
--- http-facade-client/src/main/java/io/github/shoothzj/http/facade/client/HttpClientFactory.java
+++ http-facade-client/src/main/java/io/github/shoothzj/http/facade/client/HttpClientFactory.java
@@ -3,12 +3,20 @@ package io.github.shoothzj.http.facade.client;
 public class HttpClientFactory {
     public static HttpClient createHttpClient(HttpClientConfig httpClientConfig) {
         HttpClient client;
-        switch (httpClientConfig.engine()) {
+
+        HttpClientEngine engine = httpClientConfig.engine() != null
+                ? httpClientConfig.engine()
+                : detectDefaultEngine();
+
+        switch (engine) {
             case ASYNC_HTTP_CLIENT:
                 client = new AsyncHttpClient(httpClientConfig);
                 break;
             case JDK:
                 client = new JdkHttpClient(httpClientConfig);
+                break;
+            case JDK8:
+                client = new Jdk8HttpClient(httpClientConfig);
                 break;
             case OKHTTP:
                 client = new OkhttpClient(httpClientConfig);
@@ -18,5 +26,22 @@ public class HttpClientFactory {
         }
 
         return client;
+    }
+
+    /**
+     * Detects the default engine based on the availability of java.net.HttpClient.
+     * If java.net.HttpClient is available (Java 11+), return JDK. Otherwise, return HTTP_URL_CONNECTION.
+     *
+     * @return HttpClientEngine - the default engine
+     */
+    private static HttpClientEngine detectDefaultEngine() {
+        try {
+            // Attempt to load java.net.HttpClient
+            Class.forName("java.net.http.HttpClient");
+            return HttpClientEngine.JDK;
+        } catch (ClassNotFoundException e) {
+            // If java.net.HttpClient is not available (Java 8), fall back to HttpURLConnection
+            return HttpClientEngine.JDK8;
+        }
     }
 }
