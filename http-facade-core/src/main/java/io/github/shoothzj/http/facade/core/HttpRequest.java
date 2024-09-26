7da903872a8d8018b652062fdc06470e3d7793dd
ZhangJian He <shoothzj@gmail.com>
2024-09-26 10:59:40 +0800

    feat: use empty byte array instead of null in http response (#27)
    Signed-off-by: ZhangJian He <shoothzj@gmail.com>

--- http-facade-core/src/main/java/io/github/shoothzj/http/facade/core/HttpRequest.java
+++ http-facade-core/src/main/java/io/github/shoothzj/http/facade/core/HttpRequest.java
@@ -1,6 +1,7 @@
 package io.github.shoothzj.http.facade.core;
 
 import lombok.Setter;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
@@ -9,49 +10,57 @@ import java.util.Map;
 
 @Setter
 public class HttpRequest {
+    @NotNull
     private final String url;
 
+    @NotNull
     private final HttpMethod method;
 
+    @NotNull
     private final Map<String, List<String>> headers;
 
     @Nullable
     private byte[] body;
 
-    public HttpRequest(String url, HttpMethod method) {
+    public HttpRequest(@NotNull String url, @NotNull HttpMethod method) {
         this.url = url;
         this.method = method;
         this.headers = new HashMap<>();
     }
 
-    public HttpRequest(String url, HttpMethod method, Map<String, List<String>> headers) {
+    public HttpRequest(@NotNull String url, @NotNull HttpMethod method,
+                       @NotNull Map<String, List<String>> headers) {
         this.url = url;
         this.method = method;
         this.headers = headers;
     }
 
-    public HttpRequest(String url, HttpMethod method, @Nullable byte[] body) {
+    public HttpRequest(@NotNull String url, @NotNull HttpMethod method, @Nullable byte[] body) {
         this.url = url;
         this.method = method;
         this.headers = new HashMap<>();
         this.body = body;
     }
 
-    public HttpRequest(String url, HttpMethod method, Map<String, List<String>> headers, @Nullable byte[] body) {
+    public HttpRequest(@NotNull String url, @NotNull HttpMethod method, @NotNull Map<String, List<String>> headers,
+                       @Nullable byte[] body) {
         this.url = url;
         this.method = method;
         this.headers = headers;
         this.body = body;
     }
 
+    @NotNull
     public String url() {
         return url;
     }
 
+    @NotNull
     public HttpMethod method() {
         return method;
     }
 
+    @NotNull
     public Map<String, List<String>> headers() {
         return headers;
     }
