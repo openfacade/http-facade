004c83f9e2ec58c6c5f3144fca66e92a597a013e
CennyMo <39120215+CennyMo@users.noreply.github.com>
2024-11-10 19:29:54 +0800

    feat: support reactor client (#48)
    Signed-off-by: moxiaoying <1159230165@qq.com>
    Co-authored-by: ZhangJian He <shoothzj@gmail.com>

---
+++ http-facade/src/main/java/io/github/openfacade/http/ReactorSslContextFactory.java
@@ -0,0 +1,44 @@
+package io.github.openfacade.http;
+
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import java.util.Arrays;
+
+public class ReactorSslContextFactory {
+    public static SslContext buildFromJks(String keyStorePath,
+                                          char[] keyStorePassword,
+                                          String trustStorePath,
+                                          char[] trustStorePassword,
+                                          boolean disableSslVerify,
+                                          String[] tlsProtocols,
+                                          String[] tlsCiphers) {
+
+        try {
+            SslContextBuilder sslContextBuilder = SslContextBuilder.forClient();
+
+            if (keyStorePath != null && keyStorePassword != null) {
+                sslContextBuilder.keyManager(TlsUtil.initKeyManagerFactory(keyStorePath, keyStorePassword));
+            }
+
+            if (disableSslVerify) {
+                sslContextBuilder.trustManager(InsecureTrustManagerFactory.INSTANCE);
+            } else if (trustStorePath != null && trustStorePassword != null) {
+                sslContextBuilder.trustManager(TlsUtil.initTrustManagerFactory(trustStorePath, trustStorePassword));
+            }
+
+            if (tlsProtocols != null) {
+                sslContextBuilder.protocols(tlsProtocols);
+            }
+
+            if (tlsCiphers != null) {
+                sslContextBuilder.ciphers(Arrays.asList(tlsCiphers));
+            }
+
+            return sslContextBuilder.build();
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to set up tls configuration", e);
+        }
+    }
+}
